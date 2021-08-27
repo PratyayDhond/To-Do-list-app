dc7f2ec2665ce3d5180af5608b8a156b1530b8a0
Pratyay Dhond <dhondpratyay@gmail.com>
2021-08-27 13:30:51 +0530

    Create Home Screen file and directory

---
+++ to_do_app/lib/views/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget{
+  @override
+  createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+
+    );
+  }
+}
